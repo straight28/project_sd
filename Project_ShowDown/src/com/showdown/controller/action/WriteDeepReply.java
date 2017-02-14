f8689e3645911a5a66f2376fafee8a6ad1c36446
straight28 <poem32@naver.com>
2017-02-15 00:27:33 +0900

    ///17.02.15 tonight // 입력, 수정시 sendredirect로 수정

--- Project_ShowDown/src/com/showdown/controller/action/WriteDeepReply.java
+++ Project_ShowDown/src/com/showdown/controller/action/WriteDeepReply.java
@@ -15,7 +15,7 @@ import com.showdown.dto.MemberDto;
 public class WriteDeepReply implements ActionInterface {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String url = "DO?command=board_view&num=";
 		
 		int commentnum = Integer.parseInt(request.getParameter("comment"));
 		System.out.println("코멘트번호 내용"+request.getParameter("comment"));
@@ -38,8 +38,18 @@ public class WriteDeepReply implements ActionInterface {
 		bcDTO.setRe_level(Integer.parseInt(request.getParameter("level")));
 		/* 댓글 추가  */
 		bDAO.InsertTwoBoardComment(bcDTO);
+		url += request.getParameter("num");
 		
+		/*new ViewBoardAction().execute(request, response);*/
 		
-		new ViewBoardAction().execute(request, response);
+		response.sendRedirect(url);
+		
+		/* 
+		
+		 글을 등록 하거나 수정한 후에는 forward 로 보내면 안됨. redirect 로 보내는 것을 권장.
+		forward 로 보내면 F5를 누르면 기존에 등록된 데이터가 반복되어서 등록되거나 수정됨 
+		이 부분과 관련하여 해결법 찾아보기  ==> response.sendRedirect(url);
+		
+		*/
 	}
 }
